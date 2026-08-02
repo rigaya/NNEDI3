b307f6869056f397c0ce0ce322c2ddfad2c7489c
rigaya <rigaya34589@live.jp>
2026-08-02 10:03:26 +0900

    古いmsvcp140.dll環境でのReleaseクラッシュを回避する。
    Releaseを/MTにし、MSVCのconstexpr mutex非互換対策マクロを追加する。

--- nnedi3/ThreadPool.cpp
+++ nnedi3/ThreadPool.cpp
@@ -289,8 +289,8 @@ ThreadPool::ThreadPool(void): MT_Thread(),
   CurrentThreadsUsed(0)
 {
 	for (int i = 0; i < MAX_MT_THREADS; i++) {
-		nextJob.push_back(unique_event(nullptr, nullptr));
-		jobFinished.push_back(unique_event(nullptr, nullptr));
+		nextJob.push_back(unique_event(nullptr, CloseEvent));
+		jobFinished.push_back(unique_event(nullptr, CloseEvent));
 	}
 	for (int16_t i = 0; i < MAX_MT_THREADS; i++)
 	{
