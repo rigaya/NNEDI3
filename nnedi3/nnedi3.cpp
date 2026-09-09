9023f5a258936c0cc3231d67c3c6c38e7ea82005
rigaya <rigaya34589@live.jp>
2026-09-10 07:51:35 +0900

    AVX-VNNI非対応ツールチェインではAVX2実装へフォールバックする

--- nnedi3/nnedi3.cpp
+++ nnedi3/nnedi3.cpp
@@ -330,12 +330,15 @@ static int detectVnniCpuFlags()
 	__cpuidex(leaf7, 7, 0);
 	int result = (leaf7[2] & (1 << 11)) != 0
 		? nnedi3_backend::CPU_AVX512VNNI : 0;
+#if !defined(NNEDI3_DISABLE_AVXVNNI)
+	// ビルド時にAVX-VNNIを無効化した場合は、CPUが対応していても報告しない
 	if (leaf7[0] >= 1)
 	{
 		__cpuidex(leaf7, 7, 1);
 		if ((leaf7[0] & (1 << 4)) != 0)
 			result |= nnedi3_backend::CPU_AVXVNNI;
 	}
+#endif
 	return result;
 #else
 	const unsigned int maxLeaf = __get_cpuid_max(0, nullptr);
@@ -344,9 +347,12 @@ static int detectVnniCpuFlags()
 	if (__get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx) == 0) return 0;
 	int result = (ecx & (1u << 11)) != 0
 		? nnedi3_backend::CPU_AVX512VNNI : 0;
+#if !defined(NNEDI3_DISABLE_AVXVNNI)
+	// ビルド時にAVX-VNNIを無効化した場合は、CPUが対応していても報告しない
 	if (eax >= 1 && __get_cpuid_count(7, 1, &eax, &ebx, &ecx, &edx) != 0
 		&& (eax & (1u << 4)) != 0)
 		result |= nnedi3_backend::CPU_AVXVNNI;
+#endif
 	return result;
 #endif
 }
